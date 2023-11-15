397a0e330cf9af363d7840eca65c367f61a4eed2
nikolayhotim1 <hotim_nikolay@mail.ru>
2023-11-16 00:38:40 +0300

    feature: auth & database

---
+++ convex/auth.config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+	providers: [
+		{
+			domain: 'https://wondrous-shad-48.clerk.accounts.dev',
+			applicationID: 'convex'
+		}
+	]
+}
