995a39f247835b288afd121925fae1cd572e72c1
nikolayhotim1 <hotim_nikolay@mail.ru>
2023-11-19 01:31:54 +0300

    feature: create sidebar

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { ReactNode } from 'react'
+import { useConvexAuth } from 'convex/react'
+import Spinner from '@/components/spinner'
+import { redirect } from 'next/navigation'
+import Navigation from './_components/navigation'
+
+export default function Mainlayout({ children }: { children: ReactNode }) {
+	const { isAuthenticated, isLoading } = useConvexAuth()
+	if (isLoading) {
+		return (
+			<div className='h-full flex items-center justify-center'>
+				<Spinner size='lg' />
+			</div>
+		)
+	}
+	if (!isAuthenticated) return redirect('/')
+	return (
+		<div className='h-full flex dark:bg-[#1F1F1F]'>
+			<Navigation />
+			<main className='flex-1 h-full overflow-auto'>{children}</main>
+		</div>
+	)
+}
